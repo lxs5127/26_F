c3f35066a435844cea6961548723b4b78de7402b
lxs5127 <33397342338@qq.com>
2026-09-22 11:19:03 +0800

    chore: 忽略 Debug/ 构建产物并从版本库移除

--- Debug/Drivers/MPU6050/inv_mpu.d
+++
@@ -1,8 +0,0 @@
-# FIXED
-
-Drivers/MPU6050/inv_mpu.o: ../Drivers/MPU6050/inv_mpu.c \
- ../Drivers/MPU6050/inv_mpu.h ../Drivers/MPU6050/mspm0_i2c.h \
- ../Drivers/MPU6050/mspm0_clock.h
-../Drivers/MPU6050/inv_mpu.h:
-../Drivers/MPU6050/mspm0_i2c.h:
-../Drivers/MPU6050/mspm0_clock.h:
--- Debug/Drivers/MPU6050/kalman_filter.d
+++
@@ -1,5 +0,0 @@
-# FIXED
-
-Drivers/MPU6050/kalman_filter.o: ../Drivers/MPU6050/kalman_filter.c \
- ../Drivers/MPU6050/kalman_filter.h
-../Drivers/MPU6050/kalman_filter.h:
